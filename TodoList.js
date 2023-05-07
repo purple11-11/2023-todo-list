e276f8ccb818e5dad29e95a56839ea8d07635e59
young <dbsduddls95@naver.com>
2023-05-08 03:44:36 +0900

    validation check alert, 체크박스, 수정, 삭제버튼 생성/ 취소선 미완성

--- TodoList.js
+++ TodoList.js
@@ -13,23 +13,42 @@ function addList() {
 */
 
 // <form> 태그, 화살표 함수 피드백 후 수정
-const todoList = document.querySelector("form");
-let addInput = document.querySelector("input");
-let addList = document.getElementById("result")
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const ul = document.querySelector("ul");
 
-todoList.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let list = document.createElement("li");
-    list.innerHTML = addInput.value;
-    addList.appendChild(list);
-    addInput.value = ''; // 입력 후 빈값으로 초기화
-})
-
-const isEmpty = function(value){
-    if (value === " " || value === null || value === undefined 
-    || (value !== null && typeof value === "object" && !Object.keys(value).length)) {
-        return true
-    } else {
-        return false
-    }
+const addButton = (a, b) => {
+  a.innerHTML = b;
+  ul.append(a);
 };
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (input.value.trim() === "") {
+    alert("할 일을 입력해 주세요."); // 값을 입력 안했을 때 alert
+  } else {
+    let list = document.createElement("li");
+    const complete = document.createElement("input");
+    const insert = document.createElement("button");
+    const erase = document.createElement("button");
+    // 체크박스 생성
+    complete.type = "checkbox";
+    ul.appendChild(complete);
+    // 할일 목록 생성
+    addButton(list, input.value);
+    input.value = ""; // 입력 후 빈값으로 초기화
+    // 수정, 삭제 버튼 생성
+    addButton(insert, "수정");
+    addButton(erase, "삭제");
+  }
+});
+
+// 취소선...만들고싶은데....
+complete.addEventListener("change", () => {
+  if (complete.checked === true) {
+    list.style.textDecoration = "line-through";
+  } else {
+    list.style.textDecoration = "";
+  }
+});
